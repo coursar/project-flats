8d028b867c023297b6b723ecb9a7f106beeee937
coursar <coursar@localhost>
2020-11-24 11:23:35 +0300

    demo data & requests added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,4 +4,5 @@ INSERT INTO owners (id, name) VALUES
 
 INSERT INTO flats (owner_id, district, price, rooms) VALUES
 (1, 'Ново-Савиновский', 3, 1),
-(2, 'Кировский', 5, 3);
+(2, 'Кировский', 5, 3),
+(2, 'Кировский', 3, 3);
